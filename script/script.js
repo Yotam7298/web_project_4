0cdf4e2579ae04b533f09485a2e6cc92d38c27d8
Yotam7298 <yoyo729800@gmail.com>
2022-04-07 15:13:58 +0300

    feature/element_reworked

--- script/script.js
+++ script/script.js
@@ -8,6 +8,32 @@ const currentAbout = container.querySelector(".profile__about");
 const nameField = editForm.querySelector("#name");
 const aboutField = editForm.querySelector("#about");
 // const likeButton = container.querySelectorAll(".element__like-button");
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
 // function handleLikeButton() {
 //   likeButton.src = "./images/like-button_active.svg";
@@ -30,7 +56,25 @@ function handleSaveButton(event) {
   handleCloseButton();
 }
 
+function createElement(link, name) {
+  const elementTemplate = document.querySelector("#element").content;
+  const elementsList = document.querySelector(".elements__list");
+
+  const element = elementTemplate
+    .querySelector(".elements__list-item")
+    .cloneNode(true);
+
+  element.querySelector(".element__image").src = link;
+  element.querySelector(".element__caption-text").textContent = name;
+
+  elementsList.prepend(element);
+}
+
 // likeButton.addEventListener("click", handleLikeButton);
 editButton.addEventListener("click", handleEditButton);
 closeButton.addEventListener("click", handleCloseButton);
 editForm.addEventListener("submit", handleSaveButton);
+
+initialCards.forEach((card) => {
+  createElement(card.link, card.name);
+});
