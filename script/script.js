818b417fe8623e40d03f1613e4f4c2f636ebb402
Yotam7298 <yoyo729800@gmail.com>
2022-04-07 18:08:55 +0300

    feature/image_popup_added

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector(".page");
-const editForm = container.querySelector(".popup");
+const editForm = container.querySelector(".edit-popup");
 const editButton = container.querySelector(".profile__edit-button");
-const closeButton = editForm.querySelector(".popup__close-button");
+const editCloseButton = editForm.querySelector(".edit-popup__close-button");
+const imageCloseButton = container.querySelector(".image-popup__close-button");
 const saveButton = editForm.querySelector(".form__save");
 const currentName = container.querySelector(".profile__name-text");
 const currentAbout = container.querySelector(".profile__about");
@@ -10,43 +11,43 @@ const aboutField = editForm.querySelector("#about");
 // const likeButton = container.querySelectorAll(".element__like-button");
 const initialCards = [
   {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
   {
     name: "Vanoise National Park",
     link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
   },
   {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
 ];
 
-// function handleLikeButton() {
-//   likeButton.src = "./images/like-button_active.svg";
-// }
+initialCards.forEach((card) => {
+  createElement(card.link, card.name);
+});
 
 function handleEditButton() {
   nameField.value = currentName.textContent;
   aboutField.value = currentAbout.textContent;
-  editForm.classList.add("popup_opened");
+  editForm.classList.add("edit-popup_opened");
 }
 
-function handleCloseButton() {
-  editForm.classList.remove("popup_opened");
+function handleEditCloseButton() {
+  editForm.classList.remove("edit-popup_opened");
 }
 
 function handleSaveButton(event) {
@@ -54,6 +55,12 @@ function handleSaveButton(event) {
   currentName.textContent = nameField.value;
   currentAbout.textContent = aboutField.value;
   handleCloseButton();
+}
+
+function handleImageCloseButton() {
+  container
+    .querySelector(".image-popup")
+    .classList.remove("image-popup_opened");
 }
 
 function createElement(link, name) {
@@ -67,14 +74,22 @@ function createElement(link, name) {
   element.querySelector(".element__image").src = link;
   element.querySelector(".element__caption-text").textContent = name;
 
+  element.querySelector(".element__image").addEventListener("click", (evt) => {
+    container.querySelector(".image-popup").classList.add("image-popup_opened");
+    container.querySelector(".image-popup__image").src = evt.target.src;
+  });
+
+  element
+    .querySelector(".element__remove-button")
+    .addEventListener("click", () => {
+      element.remove();
+    });
+
   elementsList.prepend(element);
 }
 
 // likeButton.addEventListener("click", handleLikeButton);
 editButton.addEventListener("click", handleEditButton);
-closeButton.addEventListener("click", handleCloseButton);
+editCloseButton.addEventListener("click", handleEditCloseButton);
 editForm.addEventListener("submit", handleSaveButton);
-
-initialCards.forEach((card) => {
-  createElement(card.link, card.name);
-});
+imageCloseButton.addEventListener("click", handleImageCloseButton);
